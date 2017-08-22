07bbec48e054384f90b79a5b802bcadf99a52deb
Adappt <sowmya.s@outlook.in>
2017-08-22 19:06:13 +0530

    removed br tag from form and property of border

--- script/script.js
+++ script/script.js
@@ -1,12 +1,11 @@
-var API_KEY="http://api.themoviedb.org/3/discover/movie?api_key=22d8568621673d1336e8d0d1fb253821";
+var API_KEY="http://api.themoviedb.org/3/discover/movie";
 var POSTER_PATH="https://image.tmdb.org/t/p/w500";
 var KEY="?api_key=22d8568621673d1336e8d0d1fb253821";
 var URL="http://api.themoviedb.org/3/movie/";
 
 (function ($) {
-	$.getJSON(API_KEY).then(function (data) {
+	$.getJSON(API_KEY+KEY).then(function (data) {
 		$.each(data.results,function(key,value){
-			console.log(data);
 			$("#movieDetails").append('<li><a href="#" onClick="callMovie('+value.id+'); return false;">' + value.title + '</a></li>');	
 		});
 	});
@@ -17,43 +16,34 @@ function callMovie(movieID){
 	 $("#right").css("width","50%");
 	 $.getJSON(URL+movieID+KEY).then(function (data) {
 	 var image=POSTER_PATH+data.backdrop_path;
-	 $("#movPoster").attr("src",image).css("border-width","thin");
-	 $("#movPoster").attr("src",image).css("border-color","black");
-	 $("#movPoster").attr("src",image).css("border-style","solid");	
+	 $("#movPoster").attr("src",image).css({"border-width":"thin","border-color":"black","border-style":"solid"});
 	 $("#title").html("<b>MOVIE TITLE </b>: " +data.title);	
 	 $("#language").html("<b>MOVIE LANGUAGE </b> : " +data.original_language);
 	 $("#releaseDate").html("<b>RELEASE DATE </b>:" +data.release_date);
 	 $("#popularity").html("<b>POPULARITY </b>:" +data.popularity);
 	 $("#overview").html("<b>OVERVIEW </b>:"+data.overview);
-	 console.log("image:"+image);
 	});
 }
 
 function aboutUs(aboutId){
 	$.getJSON("json/abtjs.json",function(data){
 	$.each(data.about,function(key,value){
-			console.log(key,value);
-			$("#home").html(aboutWrap(value.title,value.contentOne));
+		$("#home").html(aboutWrap(value.title,value.contentOne));
 	});
 		});
 }
 
 function aboutWrap(title,contentOne){
-	var output= '<div class="about"> <span class="title"> '+title+'<br> </span> <span class="contentOne">' +contentOne+'</span> </div>';
+	var output= '<div class="about"> <span class="title"> '+title+'<br> \
+	</span> <span class="contentOne">' +contentOne+'</span> </div>';
 	return output;
 }
 function contactUs(contactusId){
 	$("#home").html(
 	$("<form/>",{action:'#', method:'#'}).append(   
 	$("<input/>",{type:'text',id:'vname',name:'name',placeholder:'your name'}),
-	$("<br/>"),
-	$("<br/>"),
 	$("<input/>",{type:'text',id:'vemail',name:'email',placeholder:'your email'}),
-	$("<br/>"),
-	$("<br/>"),
 	$("<textarea/>",{rows:'5px',cols:'27px',type:'text',id:'vmsg',name:'msg',placeholder:'message'}),
-	$("<br/>"),
-	$("<br/>"),
 	$("<input/>",{type:'submit',id:'submit',value:'submit'}),$("<br/>"),$("<br/>")
 	))
 }
