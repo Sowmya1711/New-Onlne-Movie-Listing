68e059b19f8549b49ba7a91b6e03ec4dc7109e07
Adappt <sowmya.s@outlook.in>
2017-08-22 17:24:51 +0530

    simplified code

--- script/script.js
+++ script/script.js
@@ -1,21 +1,22 @@
+var API_KEY="http://api.themoviedb.org/3/discover/movie?api_key=22d8568621673d1336e8d0d1fb253821";
+var POSTER_PATH="https://image.tmdb.org/t/p/w500";
+var KEY="?api_key=22d8568621673d1336e8d0d1fb253821";
+var URL="http://api.themoviedb.org/3/movie/";
+
 (function ($) {
-	$.getJSON("http://api.themoviedb.org/3/discover/movie?api_key=22d8568621673d1336e8d0d1fb253821").then(function (data) {
+	$.getJSON(API_KEY).then(function (data) {
 		$.each(data.results,function(key,value){
 			console.log(data);
-			$("#movieDetails").append('<li><a href="#" onClick="callMovie('+value.id+'); return false;">' + value.title + '</a></li>');
-			
+			$("#movieDetails").append('<li><a href="#" onClick="callMovie('+value.id+'); return false;">' + value.title + '</a></li>');	
 		});
-		
 	});
-
 })(jQuery);
 
 function callMovie(movieID){
-	//console.log(movieID);
 	 $("#leftPart").css("width","50%");
 	 $("#right").css("width","50%");
-	 $.getJSON("http://api.themoviedb.org/3/movie/"+movieID+"?api_key=22d8568621673d1336e8d0d1fb253821").then(function (data) {
-	 var image="https://image.tmdb.org/t/p/w500"+data.backdrop_path;
+	 $.getJSON(URL+movieID+KEY).then(function (data) {
+	 var image=POSTER_PATH+data.backdrop_path;
 	 $("#movPoster").attr("src",image).css("border-width","thin");
 	 $("#movPoster").attr("src",image).css("border-color","black");
 	 $("#movPoster").attr("src",image).css("border-style","solid");	
@@ -28,8 +29,6 @@ function callMovie(movieID){
 	});
 }
 
-
-// about
 function aboutUs(aboutId){
 	$.getJSON("json/abtjs.json",function(data){
 	$.each(data.about,function(key,value){
@@ -43,23 +42,21 @@ function aboutWrap(title,contentOne){
 	var output= '<div class="about"> <span class="title"> '+title+'<br> </span> <span class="contentOne">' +contentOne+'</span> </div>';
 	return output;
 }
-
-$(document).ready(function(){
-$("#contactUs").dblclick(function(){
-$("#home").html(
-$("<form/>",{action:'#', method:'#'}).append(   
-$("<input/>",{type:'text',id:'vname',name:'name',placeholder:'your name'}),
-$("<br/>"),
-$("<br/>"),
-$("<input/>",{type:'text',id:'vemail',name:'email',placeholder:'your email'}),
-$("<br/>"),
-$("<br/>"),
-$("<textarea/>",{rows:'5px',cols:'27px',type:'text',id:'vmsg',name:'msg',placeholder:'message'}),
-$("<br/>"),
-$("<br/>"),
-$("<input/>",{type:'submit',id:'submit',value:'submit'}),$("<br/>"),$("<br/>")
+function contactUs(contactusId){
+	$("#home").html(
+	$("<form/>",{action:'#', method:'#'}).append(   
+	$("<input/>",{type:'text',id:'vname',name:'name',placeholder:'your name'}),
+	$("<br/>"),
+	$("<br/>"),
+	$("<input/>",{type:'text',id:'vemail',name:'email',placeholder:'your email'}),
+	$("<br/>"),
+	$("<br/>"),
+	$("<textarea/>",{rows:'5px',cols:'27px',type:'text',id:'vmsg',name:'msg',placeholder:'message'}),
+	$("<br/>"),
+	$("<br/>"),
+	$("<input/>",{type:'submit',id:'submit',value:'submit'}),$("<br/>"),$("<br/>")
 	))
-});
-});
+}
+
 
 
